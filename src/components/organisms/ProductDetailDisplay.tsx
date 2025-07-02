1fe2a9351726fa834fda157bc22672c73fd9cbee
Hazal Sendogan <sendoganhazal@gmail.com>
2025-07-02 16:45:10 +0300

    ürün detayı componenti eklendi

---
+++ src/components/organisms/ProductDetailDisplay.tsx
@@ -0,0 +1,42 @@
+// components/organisms/ProductDetailDisplay.tsx
+'use client';
+
+import React from 'react';
+import { Product } from '@/types';
+import ProductImage from '../atoms/ProductImage';
+import Button from '@/components/atoms/Button';
+import { Rating } from 'primereact/rating';
+import { useCart } from '../../context/CardContext';
+
+interface ProductDetailDisplayProps {
+  product: Product;
+}
+
+
+const ProductDetailDisplay: React.FC<ProductDetailDisplayProps> = ({ product }) => {
+  const { addToCart } = useCart();
+
+  return (
+    <div className="product-detail-container">
+      <div className="image-section">
+        <ProductImage src={product.image} alt={product.title} width={400} height={400} priority />
+      </div>
+      <div className="details-section">
+        <h1 className="product-title">{product.title}</h1>
+        <div className="product-rating">
+          <Rating value={product.rating.rate} readOnly cancel={false} />
+          <span className="ml-2 text-lg">({product.rating.count} yorum)</span>
+        </div>
+        <div className="product-price">${product.price.toFixed(2)}</div>
+        <Button
+          label="Sepete Ekle"
+          icon="pi pi-shopping-cart"
+          className="p-button-primary add-to-cart-button"
+          onClick={() => addToCart(product)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetailDisplay;
