e6847ee5ced9be242407f61354f7802d9c1bd265
Hazal Sendogan <sendoganhazal@gmail.com>
2026-02-04 14:35:57 +0300

    api fetcher updated

--- src/lib/fetcher/api-fetcher.ts
+++ src/lib/fetcher/api-fetcher.ts
@@ -3,10 +3,10 @@ import { Product } from "@/lib/types/types";
 const BASE_URL = "https://fakestoreapi.com";
 export async function getProducts(): Promise<Product[]> {
   const res = await fetch("https://fakestoreapi.com/products");
+
   return res.json();
 }
 console.log("ENV BASE URL =", process.env.NEXT_PUBLIC_BASE_URL);
-
 
 export async function getProduct(id: string): Promise<Product | null> {
   const res = await fetch(
@@ -19,8 +19,23 @@ export async function getProduct(id: string): Promise<Product | null> {
   return res.json();
 }
 export async function getCategories(): Promise<string[]> {
-  const res = await fetch(`${BASE_URL}/products/categories`, {
-    cache: "force-cache",
+ const url = `${process.env.NEXT_PUBLIC_BASE_URL}/products/categories`;
+
+  const res = await fetch(url, {
+    cache: "no-store",
   });
+
+  if (!res.ok) {
+    console.error("Categories API error:", res.status);
+    return [];
+  }
+
+  const contentType = res.headers.get("content-type");
+
+  if (!contentType?.includes("application/json")) {
+    console.error("Categories response is not JSON");
+    return [];
+  }
+  
   return res.json();
 }
